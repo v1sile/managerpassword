23839c48b31aec7c6c98ac1731ce17f57005689b
v1sile <vladimirlothov528@gmail.com>
2024-09-30 09:57:55 +0300

    Initial commit

---
+++ program/password.py
@@ -0,0 +1,20 @@
+#instead of first or second, enter the name of the service
+print('The password manager welcomes you')    #welcom
+print('available to you b-firs service, c-second service') #available passwords
+a = input('which service do you need a password for?') #question
+b = ('first service') #first password
+c = ('second service') #second password
+
+
+#you can add many print password scripts, just add more variables and replace a==/_/ in the script area
+
+
+#script print password /b/
+if a==b:                   
+    print('password', b) #the output of the password under the variable b
+    
+#script print password /c/
+elif a==c:
+    print('password', c) #the output of the password under the variable c
+
+
